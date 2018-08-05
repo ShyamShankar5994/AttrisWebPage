25b82dc844637ef69f3727c789c8cdb2b7eff0cd
Shyam Shankar <shyamshankar5994@gmail.com>
2018-08-05 13:47:22 +0530

    Changes made on maps

--- html/js/map.js
+++ html/js/map.js
@@ -2,7 +2,7 @@ $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var location = new google.maps.LatLng(13.037745, 77.5673501);
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
